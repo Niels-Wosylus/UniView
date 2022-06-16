4567d1479fb0951ff0deb16a0dd4d5198ad055d3
Niels Wosylus <niels.wosylus@gmail.com>
2022-06-16 20:08:39 +0200

    Image fill element

---
+++ src/UniView/Assets/Plugins/UniView/Elements/ImageFillElement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Wosylus.UniView.Elements
+{
+    [RequireComponent(typeof(Image))]
+    public sealed class ImageFillElement : ViewElement<float>
+    {
+        [SerializeField] private Image _image = default;
+
+        public override void Display(float content)
+        {
+            _image.fillAmount = content;
+        }
+
+        public override void Clear()
+        {
+            _image.fillAmount = 0;
+        }
+        
+        private void Reset()
+        {
+            _image = GetComponent<Image>();
+        }
+    }
+}
