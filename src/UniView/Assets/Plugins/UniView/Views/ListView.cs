bcd4882f6854e0c45a465051e1d4e8fbea408b8f
Niels Wosylus <niels.wosylus@gmail.com>
2022-07-19 14:36:59 +0200

    Added listview, a simplified collection view

---
+++ src/UniView/Assets/Plugins/UniView/Views/ListView.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using Wosylus.UniView.Binding;
+
+namespace Wosylus.UniView.Views
+{
+/// <summary>
+    /// Displays a collection of items
+    /// </summary>
+    /// <typeparam name="T">The type of item to display</typeparam>
+    public abstract class ListView<T> : View<IList<T>> where T : class
+    {
+        [SerializeField] private View<T> _viewPrefab = default;
+        [SerializeField] private Transform _viewParent = default;
+        [SerializeField] private List<View<T>> _children = default;
+
+        protected override void Setup(ISetup<IList<T>> setup) 
+        {
+        }
+
+        protected override void OnInitialize()
+        {
+            foreach (var view in _children)
+            {
+
+            }
+        }
+
+        protected override void OnDispose()
+        {
+            foreach (var view in _children)
+            {
+
+            }
+        }
+
+        protected override void OnDisplay(IList<T> target)
+        {
+            if (target.Count > _children.Count)
+            {
+                var difference = target.Count - _children.Count;
+                for (int i = 0; i < difference; i++)
+                {
+                    var newView = Instantiate(_viewPrefab, _viewParent);
+                    _children.Add(newView);
+                }
+            }
+            
+            for (var i = 0; i < _children.Count; i++)
+            {
+                var view = _children[i];
+
+                if(i < target.Count)
+                    view.Display(target[i]);
+                else view.Clear();
+            }
+        }
+
+        private void Reset()
+        {
+            _viewParent = transform;
+            _children = GetComponentsInChildren<View<T>>().ToList();
+        }
+    }
+}
