82a96e3711874bc031c8dc97c5e145eaac114f1d
Niels Wosylus <niels.wosylus@gmail.com>
2022-07-21 18:32:11 +0200

    Set Active element

---
+++ src/UniView/Assets/Plugins/UniView/Elements/SetActiveElement.cs
@@ -0,0 +1,14 @@
+﻿namespace Wosylus.UniView.Elements
+{
+    public class SetActiveElement : ViewElement<bool>
+    {
+        public override void Display(bool content)
+        {
+            gameObject.SetActive(content);
+        }
+
+        public override void Clear()
+        {
+        }
+    }
+}
