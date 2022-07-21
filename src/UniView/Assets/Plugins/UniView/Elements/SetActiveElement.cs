0deb8504433bbaf742f6f8b659300692ee8b5585
Niels Wosylus <niels.wosylus@gmail.com>
2022-07-21 18:53:19 +0200

    Set active element can be inverted

--- src/UniView/Assets/Plugins/UniView/Elements/SetActiveElement.cs
+++ src/UniView/Assets/Plugins/UniView/Elements/SetActiveElement.cs
@@ -1,17 +1,27 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Wosylus.UniView.Elements
 {
     public class SetActiveElement : ViewElement<bool>
     {
+        [SerializeField] private SetActiveElementMode _mode = default;
+
         public override void Display(bool content)
         {
-            gameObject.SetActive(content);
+            var active = _mode == SetActiveElementMode.EnableWhenTrue ? content : !content;
+            gameObject.SetActive(active);
         }
 
         public override void Clear()
         {
             gameObject.SetActive(false);
         }
+    }
+    
+    public enum SetActiveElementMode
+    {
+        EnableWhenTrue,
+        DisableWhenTrue,
     }
 }
