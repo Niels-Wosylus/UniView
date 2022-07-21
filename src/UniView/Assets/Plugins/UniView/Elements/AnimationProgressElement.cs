6a9964bb3e6c424dbaae3d63a2b7613f8785c6c5
Niels Wosylus <niels.wosylus@gmail.com>
2022-07-21 19:03:01 +0200

    Animator progress element

---
+++ src/UniView/Assets/Plugins/UniView/Elements/AnimationProgressElement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace Wosylus.UniView.Elements
+{
+    public class AnimationProgressElement : ViewElement<float>
+    {
+        [SerializeField] private Animator _animator = default;
+        [SerializeField] private int _layer = default;
+
+        public override void Display(float content)
+        {
+            var state = _animator.GetCurrentAnimatorStateInfo(_layer);
+            var stateHash = state.shortNameHash;
+            _animator.speed = 0;
+            _animator.Play(stateHash, _layer, content);
+        }
+
+        public override void Clear()
+        {
+            Display(default);
+        }
+    }
+}
