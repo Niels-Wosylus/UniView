bde2771c3902c61c14982d8c67affed9284a4051
Niels Wosylus <niels.wosylus@gmail.com>
2022-07-22 16:17:40 +0200

    Delayer

---
+++ src/UniView/Assets/Delay.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using Wosylus.UniView.Binding.Content.Processors;
+
+public class Delay : ViewContentProcessor
+{
+    [SerializeField] private float _delayInSeconds = default;
+    
+    public override bool CanProcess(Type inputType)
+    {
+        return true;
+    }
+
+    public override Type GetOutputType(Type inputType)
+    {
+        return inputType;
+    }
+
+    public override void Process<T>(T content, IContentProcess process)
+    {
+        StartCoroutine(Waiting(content, process));
+    }
+
+    private IEnumerator Waiting<T>(T content, IContentProcess process)
+    {
+        yield return new WaitForSeconds(_delayInSeconds);
+        process.ContinueWith(content);
+    }
+}
